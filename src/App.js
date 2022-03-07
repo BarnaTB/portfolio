20aabc54a7920829060b982bcfc90af9663435c0
BarnaTB <barnabastb2@gmail.com>
2022-03-07 20:51:18 +0300

    Remove unused Project component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { lazy, Suspense } from 'react';
 
 const Header = lazy(() => import('./components/Header'));
 const Hero = lazy(() => import('./components/Hero'));
-const Project = lazy(() => import('./components/Project'));
+// const Project = lazy(() => import('./components/Project'));
 const Skill = lazy(() => import('./components/Skill'));
 const Contact = lazy(() => import('./components/Contact'));
 const Experience = lazy(() => import('./components/Experience'))
